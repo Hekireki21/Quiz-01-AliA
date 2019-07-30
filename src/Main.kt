4326b414f4af89ae539f0868924287fdf5343ef6
Hekireki21 <53225522+Hekireki21@users.noreply.github.com>
2019-07-30 11:11:13 -0500

    commit

---
+++ src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val a = RationalNumber()
+    val b = RationalNumber()
+    var ab = add()
+    println(ab)
+}
---
+++ src/ToDouble.kt
@@ -0,0 +1,17 @@
+fun toDouble(Numerator: Int, Denominator: Int): Double {
+    var double : Double = (Numerator.toDouble()/Denominator.toDouble())
+    return (double)
+}
+
+fun add(Numerator: Int, Denominator: Int, Numerator2: Int, Denominator2: Int): RationalNumber{
+    var Numerator
+    var Denominator
+    return RationalNumber (Numerator, Denominator)
+}
+
+
+//var a
+//    var c
+//    var b
+//    var d
+//    var e
